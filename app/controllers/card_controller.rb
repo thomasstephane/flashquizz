ca476da74d6f62b8011f82f42997e8120bfabaf6
Apprentice <apprentice@devbootcamp.com>
2013-05-17 16:57:19 -0700

    Created score and game progression

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -1,26 +1,22 @@
 get '/game/:game_id/:card_id' do |game_id, card_id|
+	@game = Game.find_by_id(game_id)
+	@user = User.find_by_id(session[:id])
 	if card_id == "final"
 		erb :final 
 	else
 		@card = Card.find_by_id(card_id)
 		@choices = [@card.answer, @card.choice2, @card.choice3, @card.choice4].shuffle
-		@game = Game.find_by_id(game_id)
 		erb :card
 	end
 end
 
 post '/game/:game_id/:card_id' do |game_id, card_id|
-	p card_id
 	@card = Card.find_by_id(card_id)
 	@game = Game.find_by_id(game_id)
+	@user_answer = params[:answer]
+	@game.progression(@card, @user_answer)
 
 	if Card.find_by_id(card_id.to_i + 1)
-		if @card.check(params[:answer])
-			@card.cards_correct += 1
-		else
-			"wrong answer"
-		end
-		@game.cards_shown += 1
 		redirect "/game/#{game_id}/#{card_id.to_i + 1}"
 	else
 		redirect "/game/#{game_id}/final"
--- app/models/game.rb
+++ app/models/game.rb
@@ -9,4 +9,22 @@ class Game < ActiveRecord::Base
   	self.cards_shown ||= 0
   	self.cards_correct ||= 0
   end
+
+  def score
+  	if self.cards_shown == 0
+  		0
+  	else
+  		@score = self.cards_correct.to_f / self.cards_shown.to_f
+  	end
+  end
+
+  def progression(card, user_answer)
+  	if card.check(user_answer)
+			self.cards_correct += 1
+		else
+			"wrong answer"
+		end
+		self.cards_shown += 1
+		self.save
+	end
 end
