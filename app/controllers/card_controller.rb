38512ebac17d4a44e344e1703d2f1c3b95e9a39a
Stephane Thomas <thomas.stephn@gmail.com>
2013-05-20 08:45:52 -0700

    space bug fixed

--- app/controllers/card_controller.rb
+++ app/controllers/card_controller.rb
@@ -14,7 +14,7 @@ post '/game/:game_id/:card_id' do |game_id, card_id|
 	@card = Card.find(card_id)
 	@game = Game.find(game_id)
 	@deck = Deck.find(@game.deck_id)
-	p @user_answer = params[:answer]
+	@user_answer = params[:answer]
 	@game.progression(@card, @user_answer)
 
 	if @deck.next_card(card_id)
